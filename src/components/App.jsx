21925bb3115873def4047afcdfe6c8812413a773
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-11 12:19:05 -0500

    added features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,36 @@ class App extends Component {
        super(props);
        this.books = [
            {title: "Ready Player One", author: "Ernest Cline"},
-           {title: 'All The Light We Cannot See', author: 'Anthony Doerr'},
-           {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurit'}
+           {title: "All The Light We Cannot See", author: "Anthony Doerr"},
+           {title: "The First and Last Freedom", author: "Jiddu Krishnamurit"}
        ];
        this.state = {
-           bookNumber: 1
+           bookNumber: 0
        };
     }
+
+    goToNextBook = () => {
+       let tempBookNumber = this.state.bookNumber;
+       tempBookNumber++;
+       if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+       }
+       this.setState({
+           bookNumber: tempBookNumber
+       });
+    }
+
+    goToPreviousBook =() => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render(){
         return(
             <div className="container-fluid">
@@ -21,6 +44,7 @@ class App extends Component {
                 <div className= "row">
                     <div className= "col-md-4">
                         {/*Button her to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className= "col-md-4">
                         {/*Display book with cover here*/}
@@ -29,6 +53,7 @@ class App extends Component {
                     </div>
                     <div className= "col-md-4">
                         {/*Button here to move to the next book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
             </div>
