357d63135c034e48bcd138c3a97554478fcad8ef
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-11 11:42:12 -0500

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
+import './App.css'
+import TitleBar from './TitleBar/TitleBar';
 
 class App extends Component {
     constructor(props){
        super(props);
-       this.state = {};
+       this.books = [
+           {title: "Ready Player One", author: "Ernest Cline"},
+           {title: 'All The Light We Cannot See', author: 'Anthony Doerr'},
+           {title: 'The First and Last Freedom', author: 'Jiddu Krishnamurit'}
+       ];
+       this.state = {
+           bookNumber: 1
+       };
     }
     render(){
         return(
-         <h1>Our React app from A Compnent File</h1>   
+            <div className="container-fluid">
+                <TitleBar />
+                <div className= "row">
+                    <div className= "col-md-4">
+                        {/*Button her to move to the previous book viewed*/}
+                    </div>
+                    <div className= "col-md-4">
+                        {/*Display book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className= "col-md-4">
+                        {/*Button here to move to the next book viewed*/}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
