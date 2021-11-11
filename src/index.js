30d52c149faba609fdeae6a54e4b686bdb580bf6
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-11 10:00:08 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import reactDom from 'react-dom'
+import App from './components/App';
 
-const jsxElement = <h1>Our React App </h1>;
-console.log(jsxElement);
-
-reactDom.render(jsxElement, document.getElementById('root'));
+reactDom.render(<App/>, document.getElementById('root'));
